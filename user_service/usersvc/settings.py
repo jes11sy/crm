0460d2a62b727a347598a395b72ec214e1c5963c
jes11sy <defouz03@gmail.com>
2025-07-07 01:59:03 +0300

    справлены настройки : все сервисы теперь используют PostgreSQL вместо SQLite

--- user_service/usersvc/settings.py
+++ user_service/usersvc/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'usersvc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'crm_production'),
+        'USER': os.environ.get('DB_USER', 'crm_user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', '1740'),
+        'HOST': os.environ.get('DB_HOST', 'postgres'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
